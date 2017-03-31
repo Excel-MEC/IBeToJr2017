5187e94110e54ab9f8b7cb5de056e745b23767d5
MidhunMenonR <mmrrlm.menon@gmail.com>
2017-03-31 17:14:04 +0530

    content added

--- js/index.js
+++ js/index.js
@@ -1,53 +1,89 @@
 var flag=0;
 
 $(document).ready(function(){
-
+	
 	$(".ibeto").click(function(){
     	if (flag==0) {
-    		$(".rules,.timeline,.contract").hide();
+    		$(".rules,.timeline,.contact").hide();
     		$(".ibeto").css("width","100%");
+    		$(".ibeto_head").css("top","20%");
+    		$(".ibeto_head").css("transform","translate(-50%,0)");
     		flag=1;
-    	}
+    		setTimeout(
+  				function(){
+    				$(".contentibeto").show();
+  			}, 1000);	
+ 	  	}
     	else{
-    		$(".rules,.timeline,.contract").show();
+    		$(".rules,.timeline,.contact").show();
     		$(".ibeto").css("width","25%");
+    		$(".ibeto_head").css("top","50%");
+    		$(".ibeto_head").css("transform","translate(-50%,-50%)");
     		flag=0;
+    		$(".contentibeto").hide();
     	}
     });
     $(".rules").click(function(){
     	if (flag==0) {
-    		$(".ibeto,.timeline,.contract").hide();
+    		$(".ibeto,.timeline,.contact").hide();
     		$(".rules").css("width","100%");
+    		$(".rules_head").css("top","3%");
+    		$(".rules_head").css("transform","translate(-50%,0)");
     		flag=1;
+    		setTimeout(
+  				function(){
+    				$(".contentrule").show();
+  			}, 1000);	
     	}
     	else{
-    		$(".ibeto,.timeline,.contract").show();
+    		$(".ibeto,.timeline,.contact").show();
     		$(".rules").css("width","25%");
+    		$(".rules_head").css("top","50%");
+    		$(".rules_head").css("transform","translate(-50%,-50%)");
     		flag=0;
+    		$(".contentrule").hide();
     	}
     });
      $(".timeline").click(function(){
     	if (flag==0) {
-    		$(".ibeto,.rules,.contract").hide();
+    		$(".ibeto,.rules,.contact").hide();
     		$(".timeline").css("width","100%");
+    		$(".timeline_head").css("top","3%");
+    		$(".timeline_head").css("transform","translate(-50%,0)");
     		flag=1;
+    		setTimeout(
+  				function(){
+    				$(".contenttime").show();
+  			}, 1000);	
     	}
     	else{
-    		$(".ibeto,.rules,.contract").show();
+    		$(".ibeto,.rules,.contact").show();
     		$(".timeline").css("width","25%");
+    		$(".timeline_head").css("top","50%");
+    		$(".timeline_head").css("transform","translate(-50%,-50%)");
     		flag=0;
+    		$(".contenttime").hide();
     	}
     });
-      $(".contract").click(function(){
+      $(".contact").click(function(){
     	if (flag==0) {
     		$(".ibeto,.timeline,.rules").hide();
-    		$(".contract").css("width","100%");
+    		$(".contact").css("width","100%");
+    		$(".contact_head").css("top","15%");
+    		$(".contact_head").css("transform","translate(-50%,0)");
     		flag=1;
+    		setTimeout(
+  				function(){
+    				$(".contentcontact").show();
+  			}, 1000);	
     	}
     	else{
     		$(".ibeto,.timeline,.rules").show();
-    		$(".contract").css("width","25%");
+    		$(".contact").css("width","25%");
+    		$(".contact_head").css("top","50%");
+    		$(".contact_head").css("transform","translate(-50%,-50%)");
     		flag=0;
+    		$(".contentcontact").hide();
     	}
     });
 	
